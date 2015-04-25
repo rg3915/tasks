0302b7283b098ed2ed715c32cf730894c88df20f
Regis da Silva <regis.santos.100@gmail.com>
2015-04-25 17:12:55 -0300

    refatorando task_archive e task_detail

--- task/urls.py
+++ task/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import patterns, include, url
-from task.views import Home, TaskView, TaskDetail
+from task.views import Home, TaskView, TaskDetail, CreateUser
 
 urlpatterns = patterns(
     '',
     url(r'^$', Home.as_view(), name='home'),
     url(r'^task/$', TaskView.as_view(), name='task'),
     url(r'^task/(?P<pk>\d+)/$', TaskDetail.as_view(), name='task_detail'),
+    url(r'^create_user/$', CreateUser.as_view(), name='create_user'),
 )
