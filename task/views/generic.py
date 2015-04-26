4866f7fa9fea2d34369804e7cf1406621dee728b
Regis da Silva <regis.santos.100@gmail.com>
2015-04-26 14:01:13 -0300

    login_required

--- task/views/generic.py
+++ task/views/generic.py
@@ -1,5 +1,7 @@
 from django.views.generic import ArchiveIndexView
 from django.views.generic import DetailView
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
 
 from task.models import Task
 
@@ -7,6 +9,11 @@ from task.models import Task
 class TaskView(ArchiveIndexView):
     model = Task
     date_field = 'created_at'
+
+    @method_decorator(login_required)
+    def get(self, request, *args, **kwargs):
+        self.queryset = Task.objects.filter(user=request.user.id)
+        return super().get(request, *args, **kwargs)
 
 
 class TaskDetail(DetailView):
