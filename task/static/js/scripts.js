0029d6ca631cfb1831762dcde795a1f0ee743d55
Regis da Silva <regis.santos.100@gmail.com>
2015-04-26 23:26:45 -0300

    more templates

---
+++ task/static/js/scripts.js
@@ -0,0 +1,9 @@
+acertaClassesUsuario = function(){
+	$("input").addClass("form-control");
+};
+
+acertaClassesTarefa = function(){
+	$("input").addClass("form-control");
+	$("textarea").addClass("form-control");
+	$("input[type=checkbox]").removeClass("form-control");
+};
